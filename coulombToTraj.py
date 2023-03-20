277379a90905d1a612e7681216a6a750c1f4128a
panthibivek <panthibivek14@gmail.com>
2023-03-20 13:13:18 +0100

    code for optimization

--- coulombToTraj.py
+++ coulombToTraj.py
@@ -8,7 +8,7 @@ from formatData import loadData
 from sklearn.model_selection import train_test_split
 
 # Define the input and output dimensions
-input_dim = 78
+input_dim = 78 
 output_dim = 12 * 3
 
 def getModel():
@@ -16,9 +16,9 @@ def getModel():
     model = Sequential()
     model.add(Dense(units=1024, activation='linear', input_dim=input_dim))
     model.add(Dense(units=512, activation='linear'))
-    model.add(Dense(units=256, activation='linear'))
+    model.add(Dense(units=512, activation='linear'))
     model.add(Dense(units=64, activation='linear'))
-    model.add(Dense(units=32, activation='linear'))
+    model.add(Dense(units=64, activation='linear'))
     model.add(Dense(units=output_dim, activation='linear'))
     model.compile(loss='mse', optimizer='adam')
     model.summary()
@@ -51,9 +51,23 @@ if __name__=="__main__":
     xyz_traj_filename = "/home/panthibivek/thesis/GAN_pkg/data/traj.xyz"
     print("Loading Lower Coulomb representation and respective energies")
     molRep2D, _ = loadData(12, xyz_traj_filename)
+
+    molRep2D_without_diagonal = []
+    diagonal_index = [1,3,6,10,15,21,28,36,45,55,66,78]
+    index = np.array([i-1 for i in range(1,78) if i not in diagonal_index])
+
+    for each_molRep2D in molRep2D:
+        each_molRep2D_without_diagonal = each_molRep2D[index]
+        molRep2D_without_diagonal.append(each_molRep2D_without_diagonal)
+    molRep2D_without_diagonal = np.array(molRep2D_without_diagonal)
+
+    # charge of all atoms in benzene
+    atomic_charge = np.repeat(np.array([[6,6,6,6,6,6,1,1,1,1,1,1]]), len(molRep2D), axis=0)
+    molRep2D_with_atomic_charge = np.concatenate((atomic_charge, molRep2D_without_diagonal), axis=1)
+
     flatened_xyz = np.array(getFlattenedXyz(xyz_files_dir_name))
-    X_train, X_test, y_train, y_test = train_test_split(molRep2D, flatened_xyz,
-                                   test_size=0.5)
+    X_train, X_test, y_train, y_test = train_test_split(molRep2D_with_atomic_charge, flatened_xyz,
+                                   test_size=0.7)
     model = getModel()
     model.fit(X_train, y_train, epochs=100)
 
--- reformatToXyz.py
+++ reformatToXyz.py
@@ -7,18 +7,44 @@ import os.path
 import pathlib
 from utils import *
 from qml.representations import vector_to_matrix
+from ase import Atoms
+from ase.calculators.emt import EMT
+from ase.io import write
+from ase.io import Trajectory
 
 class ReformatToXYZ:
     def __init__(self, lower_tri_coulomb_mtx : np.array):
         # Assuming that all inputs in lower_tri_coulomb_mtx are for Benzene
         if not isinstance(lower_tri_coulomb_mtx, np.ndarray):
           raise TypeError("Passed array must be numpy!")
+        self.atomic_nums = [6,6,6,6,6,6,1,1,1,1,1,1]
         self.lower_tri_coulomb_mtx = lower_tri_coulomb_mtx
 
-    def manager(self):
+    def calculateXyz(self):
         for each_coulomb_mtx in self.lower_tri_coulomb_mtx:
-            return self.retransform_1d_to_2d(each_coulomb_mtx)
+            coulomb2D = self.retransform_1d_to_2d(each_coulomb_mtx)
+            xyz = self.coulomb_matrix_to_xyz(coulomb2D)
+        return xyz
     
     def retransform_1d_to_2d(self, coulomb_mtx_1d):
         reshaped_arr = np.reshape(coulomb_mtx_1d, (78,))
-        return vector_to_matrix(reshaped_arr)
+        return np.array(vector_to_matrix(reshaped_arr))
+
+    def coulomb_matrix_to_xyz(self, coulomb_matrix):
+        # Create Atoms object
+        atoms = Atoms(numbers=self.atomic_nums, positions=None)
+
+        # Set atomic positions
+        from scipy.spatial.distance import squareform
+        from scipy.spatial.distance import pdist
+
+        distance_matrix = squareform(pdist(coulomb_matrix))
+        print(distance_matrix)
+        atoms.set_positions(distance_matrix)
+
+        # Create trajectory and add frame
+        traj = Trajectory('output.xyz', 'w', atoms)
+        traj.write(atoms)
+
+        return atoms
+
